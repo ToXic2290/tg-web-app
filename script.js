bf9f0a1451615e899a9ae58a49dd3f95b0ffb778
ToXic2290 <87765394+ToXic2290@users.noreply.github.com>
2022-06-04 23:21:23 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ let tg = window.Telegram.WebApp;
 let data = tg.initDataUnsafe;
 
 document.querySelector('#title').innerText = `Hi, ${data.user.first_name}!`;
-document.querySelector('#paragraf').innerText = `.config -> bio_url -> Enter value`;
+document.querySelector('#paragraf').innerText = `Я Токсик)`;
+document.querySelector('#paragraf').innerText = `Мне 15 лет`;
+document.querySelector('#paragraf').innerText = `Увлекаюсь написанием скриптов и модулей`;
+document.querySelector('#paragraf').innerText = `Связь со мной: https://t.me/ToXicUse`;
